5cc71b82b062751e374090066798e896d749cd55
Nathaniel Annum <NathanielA@cocobod.net>
2026-02-03 14:43:53 +0000

    babe

--- src/App.tsx
+++ src/App.tsx
@@ -17,16 +17,16 @@ export default function Page() {
       "What if I asked really nicely?",
       "Pretty please",
       "With a chocolate rice cake on top",
-      "What about a matcha frostie",
+      "What about an ice cream date?",
       "PLEASE POOKIE",
       "But :*(",
       "I am going to die",
       "Yep im dead",
-      "ok ur talking to nathan's ghost",
+      "ok ur talking to my ghost",
       "please babe",
       ":((((",
       "PRETTY PLEASE",
-      "Estoy muerto",
+      "Yie mewu",
       "No :(",
     ];
 
@@ -34,36 +34,49 @@ export default function Page() {
   };
 
   return (
-    <div className="-mt-16 flex h-screen flex-col items-center justify-center">
-      {yesPressed ? (
-        <>
-          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!! ;))</div>
-        </>
-      ) : (
-        <>
-          <img
-            className="h-[200px]"
-            src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
-          />
-          <h1 className="my-4 text-4xl">Will you be my Valentine?</h1>
-          <div className="flex items-center">
-            <button
-              className={`mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
-              style={{ fontSize: yesButtonSize }}
-              onClick={() => setYesPressed(true)}
-            >
-              Yes
-            </button>
-            <button
-              onClick={handleNoClick}
-              className=" rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
-            >
-              {noCount === 0 ? "No" : getNoButtonText()}
-            </button>
-          </div>
-        </>
-      )}
-    </div>
+   <div className="-mt-16 flex min-h-screen flex-col items-center justify-center px-4 text-center">
+  {yesPressed ? (
+    <>
+      <img
+        className="w-40 sm:w-56 md:w-64"
+        src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif"
+        alt="Bear kiss"
+      />
+      <div className="my-4 text-2xl font-bold sm:text-3xl md:text-4xl">
+        WOOOOOO!!! I love you pookie!! ;))
+      </div>
+    </>
+  ) : (
+    <>
+      <img
+        className="h-40 sm:h-48 md:h-[200px]"
+        src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
+        alt="Cute bear"
+      />
+
+      <h1 className="my-4 text-2xl font-bold sm:text-3xl md:text-4xl">
+        Maabea, will you be my Valentine?
+      </h1>
+
+      <div className="flex flex-col gap-4 sm:flex-row sm:items-center">
+        <button
+          className="rounded bg-green-500 px-6 py-3 font-bold text-white hover:bg-green-700"
+          style={{ fontSize: yesButtonSize }}
+          onClick={() => setYesPressed(true)}
+        >
+          Yes
+        </button>
+
+        <button
+          onClick={handleNoClick}
+          className="rounded bg-red-500 px-6 py-3 font-bold text-white hover:bg-red-700"
+        >
+          {noCount === 0 ? "No" : getNoButtonText()}
+        </button>
+      </div>
+    </>
+  )}
+</div>
+
   );
 }
